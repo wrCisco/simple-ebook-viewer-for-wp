13188ee756fdf1998f1bda752ccf38e5d48f71b2
Francesco Martini <wrCisco@users.noreply.github.com>
2026-04-28 00:33:22 +0200

    Reorder menu items

--- src/js/simebv-viewer.js
+++ src/js/simebv-viewer.js
@@ -679,18 +679,18 @@ export class Reader {
             this.menu.addMenuItems([
                 menuItems.get('search'),
                 menuItems.get('history'),
-                menuItems.get('showAnnotations'),
+                menuItems.get('speechSynthesis'),
                 menuItems.get('layout'),
                 menuItems.get('maxPages'),
-                menuItems.get('fontSize'),
                 menuItems.get('fontFamily'),
+                menuItems.get('fontSize'),
                 menuItems.get('textAlign'),
                 menuItems.get('lineSpacing'),
                 menuItems.get('hyphenation'),
                 menuItems.get('margins'),
                 menuItems.get('colors'),
                 menuItems.get('colorFilter'),
-                menuItems.get('speechSynthesis'),
+                menuItems.get('showAnnotations'),
                 menuItems.get('positionViewer'),
             ])
         }
