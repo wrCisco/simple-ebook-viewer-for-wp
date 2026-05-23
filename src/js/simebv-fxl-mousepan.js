5e6ede9fbc06d8c6be74fa362264e32ebbfaf109
Francesco Martini <wrCisco@users.noreply.github.com>
2026-05-24 00:18:24 +0200

    Fixes for fixed layout's mouse panning
    - more precise test if mousedown happened over text
    - don't start panning if the user clicked on an interactive
      or an explicitly draggable element
    - preventDefault on dragstart for automatic
      draggable elements (images, selections, links...)

--- src/js/simebv-fxl-mousepan.js
+++ src/js/simebv-fxl-mousepan.js
@@ -2,10 +2,27 @@ let drag = false
 let pos
 let renderer
 
+const isPointOverText = (el, x, y) => {
+    const walker = document.createTreeWalker(el, NodeFilter.SHOW_TEXT)
+    const range = document.createRange()
+    let node
+    while ((node = walker.nextNode())) {
+        if (!node.data.trim()) continue
+        range.selectNodeContents(node);
+        for (const rect of range.getClientRects()) {
+            if (x >= rect.left - 10 && x <= rect.right + 10 &&
+                    y >= rect.top - 5 && y <= rect.bottom + 5) {
+                return true
+            }
+        }
+    }
+}
+
 const onMouseDown = e => {
-    const el = e.currentTarget.elementFromPoint(e.clientX, e.clientY)
-    if (!el || Array.from(el.childNodes)
-            .some(node => node.nodeType === Node.TEXT_NODE && node.data.trim().length > 0)) {
+    const el = e.target
+    // Let the user select text or interact with interactive elements
+    if (isPointOverText(el, e.clientX, e.clientY)
+            || el.closest('a, button, input, textarea, select, [contenteditable], [draggable="true"]')) {
         return
     }
     drag = true
@@ -22,7 +39,16 @@ const onMouseMove = e => {
     }
 }
 
-const onMouseUp = e => drag = false
+const onMouseUp = e => {
+    drag = false
+}
+
+const onDragStart = e => {
+    // Avoid that automatic drag events (on images, selections...) disrupt the panning
+    if (!e.target.closest('[draggable="true"]')) {
+        e.preventDefault()
+    }
+}
 
 export const setMousePanEvents = renderer_ => {
     renderer = renderer_
@@ -30,6 +56,6 @@ export const setMousePanEvents = renderer_ => {
         doc.addEventListener('mousedown', onMouseDown)
         doc.addEventListener('mousemove', onMouseMove)
         doc.addEventListener('mouseup', onMouseUp)
-        document?.addEventListener('mouseup', onMouseUp)
+        doc.addEventListener('dragstart', onDragStart)
     }
 }
